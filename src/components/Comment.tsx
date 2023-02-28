ad1caead8e828399c51882208b62c7a7821049cd
Nitin Sirsath <73011138+NitinSirsath@users.noreply.github.com>
2023-02-28 15:22:40 +0530

    Update Comment.tsx

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -19,6 +19,12 @@ const Comment = ({commentText}: Props) => {
         <p>{commentText.body}</p>
         <button onClick={() => setIsReply(!isReply)}>{isReply? 'cancel' : 'reply'}</button>
         {isReply && <CommentInput onComment={onComment}/>}
+        <div>
+            {reply?.map((commentText) => {
+                return <Comment commentText={commentText}/>
+              
+            })}
+        </div>
     </div>
   )
 } 
